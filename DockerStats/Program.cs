06609b29be9b0d26a738a64412a6177c2195928a
madhub <b.madhu@outlook.com>
2023-10-14 12:44:25 +0530

    initial commit

---
+++ DockerStats/Program.cs
@@ -0,0 +1,85 @@
+﻿
+using CommandLine;
+using Docker.DotNet;
+using Docker.DotNet.Models;
+using DockerStats;
+using System.Runtime.InteropServices;
+
+var parsedArgs = Parser.Default.ParseArguments<DockerStatsOptions>(args);
+parsedArgs.WithNotParsed(list =>
+{
+    Environment.Exit(-1);
+});
+
+
+if (!string.IsNullOrEmpty(parsedArgs.Value.output))
+{
+    Console.WriteLine($"Writing starts to file {parsedArgs.Value.output}"); 
+    LogUtils.SetLogOut(LogUtils.LogOutType.File, parsedArgs.Value);
+}
+
+DockerClient? client = default;
+
+if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+{
+    client = new DockerClientConfiguration(
+        new Uri("npipe://./pipe/docker_engine"))
+         .CreateClient();
+}
+else
+{
+    // Default Docker Engine on Linux
+    client = new DockerClientConfiguration(
+        new Uri("unix:///var/run/docker.sock"))
+         .CreateClient();
+}
+
+// declare variable for listing containers & Getting Docker Starts
+var containerListParameters = new ContainersListParameters()
+{
+    All = true,
+    // filter for running & created containers
+    Filters = new Dictionary<string, IDictionary<string, bool>>
+    {
+        ["status"] = new Dictionary<string, bool>
+        {
+            ["running"] = true,
+            ["created"] = true
+        }
+    },
+
+};
+
+var containerStartsParams = new ContainerStatsParameters() { OneShot = true, Stream = false };
+
+// callback function to logs the receives stats from Docker Daemon
+var statsCallback = new Progress<ContainerStatsResponse>(m =>
+{
+
+    var dockerStats = Helper.ParseStats(m);
+    LogUtils.Log($"{m.Name},{dockerStats.CpuUsageInPercent:F2},{(long)dockerStats.MemoryUsageInKB},{(long)dockerStats.MemoryLimitInKB},{dockerStats.MemoryUsageInPercent:F2},{dockerStats.TimeStamp}");
+
+
+});
+
+// Write header
+LogUtils.Log($"ContainerName,CpuPercent,MemUsageInKB,MaxAviMemInKB,MemUsageInPercent,DateTime");
+while (true)
+{
+    var containers = await client.Containers.ListContainersAsync(containerListParameters);
+    foreach (var container in containers)
+    {
+        try
+        {
+            await client.Containers.GetContainerStatsAsync(container.ID, containerStartsParams, statsCallback);
+        }
+        catch (Exception exp)
+        {
+            Console.WriteLine($"Exception: {exp}");
+            Environment.Exit(-1);
+        }
+
+
+    }
+    await Task.Delay((int)TimeSpan.FromSeconds((int)parsedArgs.Value.pollfrequence).TotalMilliseconds);
+}
--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # DockerStats
-CLI tool to log Docker Stats in CSV format 
+CLI tool to log Docker Stats in CSV format using C# Docker Client SDK.
+# Source code build
+1. Checkout the DockerStats repo
+    ```shell
+    git clone https://github.com/madhub/DockerStats.git
+    ```  
+2. Build self contained executable that work on windows & Linux
+      * Windows Build
+        ```shell
+        // Binary will be published to bin\Release\net6.0\publish\win-x64\
+        dotnet publish -c Release -p:PublishProfile=WinProfile
+        ```  
+    * Linux Build
+        ```shell
+        // Binary will be published to bin\Release\net6.0\publish\linux-x64 directory
+        dotnet publish -c Release -p:PublishProfile=LinuxProfile
+        ```  
+3. Run command
+    ```shell
+        DockerStats --help
+    ```
